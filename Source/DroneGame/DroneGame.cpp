a9175699e9ed2698d13b5b8ff3c7d20d9e367312
user <email@example.com>
2024-07-11 16:47:04 +0300

    Init

---
+++ Source/DroneGame/DroneGame.cpp
@@ -0,0 +1,4 @@
+#include "DroneGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DroneGame, "DroneGame" );
---
+++ Source/DroneGame/GameplayGameMode.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameplayGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRONEGAME_API AGameplayGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
